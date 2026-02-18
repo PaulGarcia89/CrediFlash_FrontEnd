a4279687050da0ad23faa5876a1f52bc74f6aa46
PaulGarcia89 <34041867+PaulGarcia89@users.noreply.github.com>
2026-02-18 11:46:24 -0500

    Prepare frontend for Vercel

---
+++ .next/dev/server/chunks/[root-of-the-server]__d1714471._.js
@@ -0,0 +1,87 @@
+module.exports = [
+"[externals]/next/dist/server/app-render/work-async-storage.external.js [external] (next/dist/server/app-render/work-async-storage.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/app-render/work-async-storage.external.js", () => require("next/dist/server/app-render/work-async-storage.external.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/compiled/@opentelemetry/api [external] (next/dist/compiled/@opentelemetry/api, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/compiled/@opentelemetry/api", () => require("next/dist/compiled/@opentelemetry/api"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/server/app-render/work-unit-async-storage.external.js [external] (next/dist/server/app-render/work-unit-async-storage.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/app-render/work-unit-async-storage.external.js", () => require("next/dist/server/app-render/work-unit-async-storage.external.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/server/lib/incremental-cache/tags-manifest.external.js [external] (next/dist/server/lib/incremental-cache/tags-manifest.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/lib/incremental-cache/tags-manifest.external.js", () => require("next/dist/server/lib/incremental-cache/tags-manifest.external.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/server/app-render/after-task-async-storage.external.js [external] (next/dist/server/app-render/after-task-async-storage.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/app-render/after-task-async-storage.external.js", () => require("next/dist/server/app-render/after-task-async-storage.external.js"));
+
+module.exports = mod;
+}),
+"[externals]/node:async_hooks [external] (node:async_hooks, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("node:async_hooks", () => require("node:async_hooks"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/compiled/next-server/app-page-turbo.runtime.dev.js [external] (next/dist/compiled/next-server/app-page-turbo.runtime.dev.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/compiled/next-server/app-page-turbo.runtime.dev.js", () => require("next/dist/compiled/next-server/app-page-turbo.runtime.dev.js"));
+
+module.exports = mod;
+}),
+"[project]/src/proxy.js [middleware] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "config",
+    ()=>config,
+    "proxy",
+    ()=>proxy
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$server$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/server.js [middleware] (ecmascript)");
+;
+const PUBLIC_PATH_PREFIXES = [
+    '/login',
+    '/register-analista',
+    '/authentication',
+    '/images'
+];
+const isPublicPath = (pathname)=>PUBLIC_PATH_PREFIXES.some((prefix)=>pathname === prefix || pathname.startsWith(`${prefix}/`));
+function proxy(request) {
+    const { pathname, search } = request.nextUrl;
+    if (pathname === '/') {
+        return __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$server$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__["NextResponse"].redirect(new URL('/login', request.url));
+    }
+    if (isPublicPath(pathname)) {
+        return __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$server$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__["NextResponse"].next();
+    }
+    const token = request.cookies.get('cf_token')?.value;
+    if (!token) {
+        const redirect = `${pathname}${search || ''}`;
+        const loginUrl = new URL('/login', request.url);
+        loginUrl.searchParams.set('redirect', redirect);
+        return __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$server$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__["NextResponse"].redirect(loginUrl);
+    }
+    return __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$server$2e$js__$5b$middleware$5d$__$28$ecmascript$29$__["NextResponse"].next();
+}
+const config = {
+    matcher: [
+        '/((?!_next/static|_next/image|favicon.ico|images/).*)'
+    ]
+};
+}),
+];
+
+//# sourceMappingURL=%5Broot-of-the-server%5D__d1714471._.js.map
--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,49 +1,11 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/2d6a7_next_dist_compiled_bf6b6693._.js",
-      "static/chunks/2d6a7_next_dist_shared_lib_45974c0b._.js",
-      "static/chunks/2d6a7_next_dist_client_a029879a._.js",
-      "static/chunks/2d6a7_next_dist_47267219._.js",
-      "static/chunks/2d6a7_next_app_2e5cfe47.js",
-      "static/chunks/[next]_entry_page-loader_ts_00c52d00._.js",
-      "static/chunks/2d6a7_react-dom_8288efa7._.js",
-      "static/chunks/2d6a7_dd1abeab._.js",
-      "static/chunks/[root-of-the-server]__c1be6230._.js",
-      "static/chunks/crediflash-vuexy-next_pages__app_2da965e7._.js",
-      "static/chunks/turbopack-crediflash-vuexy-next_pages__app_c2e76de8._.js"
-    ],
-    "/_error": [
-      "static/chunks/2d6a7_next_dist_compiled_bf6b6693._.js",
-      "static/chunks/2d6a7_next_dist_shared_lib_1980bb0c._.js",
-      "static/chunks/2d6a7_next_dist_client_a029879a._.js",
-      "static/chunks/2d6a7_next_dist_29391d89._.js",
-      "static/chunks/2d6a7_next_error_1ac3379a.js",
-      "static/chunks/[next]_entry_page-loader_ts_a04e1f46._.js",
-      "static/chunks/2d6a7_react-dom_8288efa7._.js",
-      "static/chunks/2d6a7_dd1abeab._.js",
-      "static/chunks/[root-of-the-server]__9fc8fcc3._.js",
-      "static/chunks/crediflash-vuexy-next_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-crediflash-vuexy-next_pages__error_5fb055d3._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [
-    "static/chunks/2d6a7_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_31a6a1ad._.js",
-    "static/chunks/2d6a7_next_dist_compiled_react-dom_23c79930._.js",
-    "static/chunks/2d6a7_next_dist_compiled_react-server-dom-turbopack_98c642b8._.js",
-    "static/chunks/2d6a7_next_dist_compiled_next-devtools_index_8ea965a4.js",
-    "static/chunks/2d6a7_next_dist_compiled_56b04da3._.js",
-    "static/chunks/2d6a7_next_dist_client_a02b7100._.js",
-    "static/chunks/2d6a7_next_dist_e7e6a4b3._.js",
-    "static/chunks/2d6a7_08a496e0._.js",
-    "static/chunks/crediflash-vuexy-next_a0ff3932._.js",
-    "static/chunks/turbopack-crediflash-vuexy-next_82d039ae._.js"
-  ]
+  "rootMainFiles": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
