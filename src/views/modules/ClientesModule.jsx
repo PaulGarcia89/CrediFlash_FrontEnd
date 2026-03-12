be2faef2506ac128b8b7be6c3425ee3c2e86544a
PaulGarcia89 <34041867+PaulGarcia89@users.noreply.github.com>
2026-03-12 16:52:05 -0400

    feat(clientes): agregar boton de WhatsApp en acciones sin funcionalidad

--- src/views/modules/ClientesModule.jsx
+++ src/views/modules/ClientesModule.jsx
@@ -538,6 +538,11 @@ export default function ClientesModule() {
                                 </IconButton>
                               </span>
                             </Tooltip>
+                            <Tooltip title='WhatsApp (próximamente)'>
+                              <IconButton size='small' color='success' onClick={() => {}}>
+                                <i className='tabler-brand-whatsapp text-3xl' />
+                              </IconButton>
+                            </Tooltip>
                           </Stack>
                         </TableCell>
                       </TableRow>
